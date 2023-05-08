cbfc883b47fb4f068b719feca1c7cad557c84365
mabisha164 <mabisha164@gmail.com>
2023-05-08 23:40:21 +0545

    New Commit

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+#this is test
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('students.urls'))
+    path('',include('students.urls')),
 ]
